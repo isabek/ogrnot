442f7e8c44a64f06af9578e79b9549177442876e
Isabek <isabek2309@gmail.com>
2015-03-03 19:02:04 +0600

    Added Rest Client implementation class

---
+++ app/src/main/java/com/itashiev/ogrnot/ogrnotapplication/RESTClient/OgrnotRestClient.java
@@ -0,0 +1,28 @@
+package com.itashiev.ogrnot.ogrnotapplication.RESTClient;
+
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+public class OgrnotRestClient {
+    private static final String BASE_URL = "/api/v1/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void put(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
