dd70ab904c9aa26a6be548ad6934c7b1f9a1ede2
imanali <davit.mr@mail.ru>
2015-03-04 11:05:09 +0600

    Impemented Post for login

--- app/src/main/java/com/itashiev/ogrnot/ogrnotapplication/RESTClient/OgrnotRestClient.java
+++ app/src/main/java/com/itashiev/ogrnot/ogrnotapplication/RESTClient/OgrnotRestClient.java
@@ -3,10 +3,11 @@ package com.itashiev.ogrnot.ogrnotapplication.RESTClient;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 public class OgrnotRestClient {
-    private static final String BASE_URL = "/api/v1/";
+    private static final String BASE_URL = "http://192.168.1.137:8080/api/v1/";
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
@@ -15,11 +16,11 @@ public class OgrnotRestClient {
     }
 
     public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.get(getAbsoluteUrl(url), params, responseHandler);
+        client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
     public static void put(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.get(getAbsoluteUrl(url), params, responseHandler);
+        client.put(getAbsoluteUrl(url), params, responseHandler);
     }
 
     private static String getAbsoluteUrl(String relativeUrl) {
--- app/src/main/java/com/itashiev/ogrnot/ogrnotapplication/activities/LoginActivity.java
+++ app/src/main/java/com/itashiev/ogrnot/ogrnotapplication/activities/LoginActivity.java
@@ -11,8 +11,19 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 
 import com.itashiev.ogrnot.ogrnotapplication.R;
+import com.itashiev.ogrnot.ogrnotapplication.RESTClient.OgrnotRestClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
@@ -20,19 +31,55 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     private EditText studentNumberEditTextView;
     private EditText passwordEditTextView;
     private Button signInButtonView;
+    private ProgressBar loginProgressBar;
+    private ScrollView loginFormScrollView;
+    private ImageView logoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        studentNumberEditTextView  = (EditText)findViewById(R.id.student_number);
-        passwordEditTextView = (EditText)findViewById(R.id.password);
+        studentNumberEditTextView = (EditText) findViewById(R.id.student_number);
+        passwordEditTextView = (EditText) findViewById(R.id.password);
         signInButtonView = (Button) findViewById(R.id.button_sign_in);
+        loginProgressBar = (ProgressBar)findViewById(R.id.login_progress);
+        loginFormScrollView = (ScrollView) findViewById(R.id.login_form);
+        logoImageView = (ImageView)findViewById(R.id.logo);
 
         signInButtonView.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String studentNumber = studentNumberEditTextView.getText().toString();
+                String password = passwordEditTextView.getText().toString();
+
+                System.out.println(studentNumber);
+                System.out.println(password);
+
+                RequestParams params = new RequestParams();
+                params.put("user", studentNumber);
+                params.put("pass", password);
+
+                OgrnotRestClient.post("authenticate", params, new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        try {
+                            String authKey = (String) response.get("authKey");
+                        } catch (JSONException e) {
+                            Log.e("LoginActivity", e.getMessage());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+                        System.out.println("2");
+                        System.out.println(responseString);
+                        System.out.println(statusCode + "");
+                    }
+                });
 
             }
         });
