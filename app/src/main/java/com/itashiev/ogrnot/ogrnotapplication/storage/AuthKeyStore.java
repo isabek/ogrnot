0569a47455601205031f972deedb08d3c22b7e3e
Isabek <isabek2309@gmail.com>
2015-03-04 14:58:03 +0600

    Added AuthKeyStore.java for store auth keys

---
+++ app/src/main/java/com/itashiev/ogrnot/ogrnotapplication/storage/AuthKeyStore.java
@@ -0,0 +1,30 @@
+package com.itashiev.ogrnot.ogrnotapplication.storage;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class AuthKeyStore {
+
+    private static String keyForAuthKey = "authKey";
+
+    private static final String SETTINGS_FILE = "OgrNotFile";
+
+    public static void setAuthKey(Context context, String authKey) {
+        SharedPreferences.Editor editor = getPreferences(context).edit();
+        editor.putString(keyForAuthKey, authKey);
+        editor.commit();
+    }
+
+    public static String getAuthKey(Context context) {
+        SharedPreferences preferences = getPreferences(context);
+
+        return preferences.getString(keyForAuthKey, null);
+    }
+
+    private static SharedPreferences getPreferences(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences;
+    }
+
+}
