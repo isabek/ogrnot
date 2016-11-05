fe62fc29e3dabd76a190aa8510afa6463f3a33ac
Isabek <isabek2309@gmail.com>
2016-11-06 00:37:34 +0300

    Issue #8 Hide application if back button pressed(if left menu closed).

--- app/src/main/java/com/itashiev/ogrnot/ogrnotapplication/activities/MainActivity.java
+++ app/src/main/java/com/itashiev/ogrnot/ogrnotapplication/activities/MainActivity.java
@@ -106,6 +106,8 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else {
+            moveTaskToBack(true);
         }
     }
 
